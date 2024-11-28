7d2afb2f5f6685d2ab203f570a1ec2eef213790a
Shyy23 <syahrulhidayatulloh14@gmail.com>
2024-11-29 05:57:14 +0700

    bg-health

--- scenes/battle-scene.js
+++ scenes/battle-scene.js
@@ -1,4 +1,4 @@
-import {BATTLE_BACKGROUND_ASSET_KEYS, MONSTER_ASSET_KEYS} from "../dist/assets/asset-keys.js";
+import {BATTLE_ASSET_KEYS, BATTLE_BACKGROUND_ASSET_KEYS, MONSTER_ASSET_KEYS} from "../dist/assets/asset-keys.js";
 import Phaser from "../dist/lib/phaser.js";
 import { SCENE_KEYS } from "./scenes-keys.js";
 
@@ -19,6 +19,20 @@ export class BattleScene extends Phaser.Scene {
         // render monster & player
         this.add.image(768, 140, MONSTER_ASSET_KEYS.JIVY, 0);
         this.add.image(256, 316, MONSTER_ASSET_KEYS.FROSTSABER, 0).setFlipX(true);
+
+        // render player health bar
+        const playerMonsterName = this.add.text(
+            30,
+            20,
+            MONSTER_ASSET_KEYS.JIVY, {
+                color: '#7e3d3f',
+                fontSize: '32px'
+            }
+        ); 
+        this.add.container(556, 318, [
+            this.add.image(0,0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
+            .setOrigin(0),
+            playerMonsterName]);
     }
 
 }
